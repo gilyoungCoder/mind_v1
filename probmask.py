04d990deb0ea1b3f34a3b7dc77bfcde84078209a
gilyoungCoder <yhgil99@naver.com>
2024-03-03 21:11:23 +0900

    probmask modify

--- probmask.py
+++ probmask.py
@@ -20,7 +20,7 @@ def probabilistic_mask_update(mask, kernel_size=3, padding=1, alpha = 10):
     neighbors_ratio = neighbors_count / kernel_size
     higher_probs = torch.sigmoid(neighbors_ratio * alpha)
     # 확률적 업데이트를 위한 마스크 생성
-    updated_mask = torch.bernoulli(neighbors_ratio)
+    updated_mask = torch.bernoulli(higher_probs)
     # updated_mask = torch.where(neighbors_ratio >= alpha, torch.ones_like(neighbors_ratio), torch.zeros_like(neighbors_ratio))
 
     
